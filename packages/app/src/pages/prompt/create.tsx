e61b96ae0edfb5378e9260250a94e065a5574efe
Dave Gorum <calculator@gmail.com>
2022-04-23 18:21:36 -0700

    Tidy up create form

--- packages/app/src/pages/prompt/create.tsx
+++ packages/app/src/pages/prompt/create.tsx
@@ -1,119 +1,139 @@
-import React, { ChangeEvent } from "react";
-import { promptyContract } from "../../contracts";
-import dayjs from "dayjs";
-import MainLayout from "../../layouts/MainLayout";
-import { useWallet } from "@gimmixorg/use-wallet";
+import React, { ChangeEvent } from 'react';
+import { promptyContract } from '../../contracts';
+import dayjs from 'dayjs';
+import MainLayout from '../../layouts/MainLayout';
+import { useWallet } from '@gimmixorg/use-wallet';
+import TextareaAutosize from 'react-textarea-autosize';
+import { ENSName } from 'react-ens-name';
 
 const Create = () => {
   const { account, provider, connect } = useWallet();
-  const [text, setText] = React.useState("");
-  const [minChars, setMinChars] = React.useState("1");
-  const [maxChars, setMaxChars] = React.useState("500");
-  const [days, setDays] = React.useState<"1" | "3" | "7">("1");
+  const [text, setText] = React.useState('');
+  const [minChars, setMinChars] = React.useState('1');
+  const [maxChars, setMaxChars] = React.useState('500');
+  const [days, setDays] = React.useState<'1' | '3' | '7'>('1');
 
   const submitPrompt = async () => {
     if (provider) {
-      console.log("provider");
+      console.log('provider');
       const signer = await promptyContract.connect(provider.getSigner());
 
       const tx = await signer.create(
         text,
-        dayjs().add(parseInt(days), "minute").unix(),
+        dayjs().add(parseInt(days), 'minute').unix(),
         minChars,
         maxChars
       );
-      console.log("tx", tx.hash);
+      console.log('tx', tx.hash);
       await tx.wait(2);
     } else {
-      console.log("no provider");
+      console.log('no provider');
     }
   };
 
   return (
     <MainLayout>
-      <div className="create">
-        <div>{account}</div>
-        <div>
-          <h1>Create a Prompt</h1>
-          {/* <form> */}
-          <textarea
-            className="border border-gray-500 p-2 mx-4"
-            onChange={(e) => setText(e.target.value)}
-            value={text}
-          />
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Min Chars
-          </label>
-          <input
-            className="border border-gray-500 p-2 mx-4"
-            value={minChars}
-            onChange={(e) => setMinChars(e.target.value)}
-          />
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Max Chars
-          </label>
-          <input
-            className="border border-gray-500 p-2 mx-4"
-            value={maxChars}
-            onChange={(e) => setMaxChars(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              !account ? connect() : submitPrompt();
-            }}
-            className="p-4 bg-blue-500 text-white"
-            type="submit"
-          >
-            {!account ? "Connect Wallet" : "Submit Prompt"}
-          </button>
-          <form
-            style={{ display: "flex" }}
-            onChange={(e) => setDays(e.target.value)}
-          >
-            <div className="form-check">
-              <input
-                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="flexRadioDefault"
-                checked={days === "1"}
-                value="1"
-              />
-              <label className="form-check-label inline-block text-gray-800">
-                1 day
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="flexRadioDefault"
-                checked={days === "3"}
-                value="3"
-              />
-              <label className="form-check-label inline-block text-gray-800">
-                3 day
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="flexRadioDefault"
-                checked={days === "7"}
-                value="7"
-              />
-              <label className="form-check-label inline-block text-gray-800">
-                7 day
-              </label>
-            </div>
-          </form>
-          {/* </form> */}
-        </div>{" "}
+      {/* <form> */}
+      <div className="relative mb-6">
+        <TextareaAutosize
+          className="w-full h-24 p-3 border-2 border-gray-200 rounded-lg pb-12 placeholder:text-gray-400"
+          minRows={6}
+          maxRows={14}
+          placeholder="Write your prompt..."
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          disabled={!account}
+        />
+        {account && (
+          <div className="absolute bottom-4 left-4 py-2 text-sm font-bold text-gray-400">
+            <ENSName address={account} />
+          </div>
+        )}
       </div>
-      <style jsx>{`
-        .create {
-        }
-      `}</style>
+
+      <form onChange={(e) => setDays(e.target.value)}>
+        <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">
+          How long should the prompt run?
+        </h4>
+        <div className="flex mb-8">
+          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="radio"
+              id="flexRadioDefault1"
+              checked={days === '1'}
+              value="1"
+            />
+            <label
+              className="form-check-label inline-block text-gray-800 cursor-pointer "
+              htmlFor="flexRadioDefault1"
+            >
+              24 hours
+            </label>
+          </div>
+          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="radio"
+              id="flexRadioDefault3"
+              checked={days === '3'}
+              value="3"
+            />
+            <label
+              className="form-check-label inline-block text-gray-800 cursor-pointer"
+              htmlFor="flexRadioDefault3"
+            >
+              3 days
+            </label>
+          </div>
+          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
+            <input
+              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="radio"
+              id="flexRadioDefault7"
+              checked={days === '7'}
+              value="7"
+            />
+            <label
+              className="form-check-label inline-block text-gray-800 cursor-pointer"
+              htmlFor="flexRadioDefault7"
+            >
+              7 days
+            </label>
+          </div>
+        </div>
+      </form>
+
+      <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">
+        What should the length of responses be?
+      </h4>
+      <div className="flex mb-8 items-center">
+        <label className="block text-gray-700 text-sm">Min Chars</label>
+        <input
+          className="border border-gray-300 p-2 mx-2 w-16"
+          value={minChars}
+          onChange={(e) => setMinChars(e.target.value)}
+        />
+        <label className="block text-gray-700 text-sm">Max Chars</label>
+        <input
+          className="border border-gray-300 p-2 mx-2 w-16"
+          value={maxChars}
+          onChange={(e) => setMaxChars(e.target.value)}
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          !account ? connect() : submitPrompt();
+        }}
+        className="rounded-full px-5 py-2 bg-orange-500 text-white text-sm font-bold"
+        type="submit"
+      >
+        {!account ? 'Connect Wallet' : 'Post your Prompt'}
+      </button>
+      {/* </form> */}
+
+      <style jsx>{``}</style>
     </MainLayout>
   );
 };
