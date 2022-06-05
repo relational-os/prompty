e07fd7499666df7923c11459e68e8cba7d763300
Arthur Tyukayev <github_arthur@tyukayev.com>
2022-06-05 06:01:56 -0700

    Added prompty submit loading state

---
+++ packages/app/src/components/RadioButton.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+type ClickHandler = (
+  // eslint-disable-next-line no-unused-vars
+  event:
+    | React.MouseEventHandler<HTMLDivElement>
+    | React.ChangeEventHandler<HTMLInputElement>,
+  // eslint-disable-next-line no-unused-vars
+  value: string
+) => void;
+
+interface RadioButtonProps {
+  label: string;
+  checked: boolean;
+  id: string;
+  value: string;
+  disabled?: boolean;
+  onClick?: ClickHandler;
+}
+
+const RadioButton = ({
+  id,
+  label,
+  checked,
+  value,
+  onClick,
+  disabled,
+}: RadioButtonProps) => {
+  const handleClick = (
+    event:
+      | React.MouseEventHandler<HTMLDivElement>
+      | React.ChangeEventHandler<HTMLInputElement>
+  ) => {
+    if (onClick && !disabled) onClick(event, value);
+  };
+
+  return (
+    <div
+      // @ts-ignore
+      onClick={handleClick}
+      className={`
+        form-check
+        rounded-lg
+        px-4
+        py-3
+        mr-4
+        ${disabled ? "focus:outline-none bg-white/50" : `bg-white`}
+      `}
+      disabled={disabled}
+    >
+      <input
+        className={`
+          form-check-input
+          appearance-none
+          rounded-full
+          h-4
+          w-4 border
+          border-gray-300
+          bg-white
+          checked:bg-blue-600
+          checked:border-blue-600
+          focus:outline-none
+          transition
+          duration-200
+          mt-1
+          align-top
+          bg-no-repeat
+          bg-center
+          bg-contain
+          float-left
+          mr-2
+          ${disabled ? "cursor-default disabled:opacity-75" : "cursor-pointer"}
+        `}
+        type="radio"
+        id={id}
+        checked={checked}
+        value={value}
+        disabled={disabled}
+        // @ts-ignore
+        onChange={handleClick}
+      />
+      <label
+        className={`
+          form-check-label
+          inline-block
+          text-gray-800
+          select-none
+          ${disabled ? "cursor-default" : "cursor-pointer"}
+        `}
+        htmlFor={id}
+      >
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default React.memo(RadioButton);
--- packages/app/src/pages/prompt/create.tsx
+++ packages/app/src/pages/prompt/create.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { ABI, PROMPTY_ADDRESS } from "../../contracts";
+import Spinner from "src/components/Spinner";
 import dayjs from "dayjs";
-import MainLayout from "../../layouts/MainLayout";
+import MainLayout from "src/layouts/MainLayout";
+import RadioButton from "src/components/RadioButton";
 import TextareaAutosize from "react-textarea-autosize";
 import { ENSName } from "react-ens-name";
 import {
@@ -15,13 +17,18 @@ import { defaultAbiCoder } from "ethers/lib/utils";
 
 const Create = () => {
   const router = useRouter();
-  const [text, setText] = React.useState("");
-  const [minChars, setMinChars] = React.useState("1");
-  const [maxChars, setMaxChars] = React.useState("500");
-  const [days, setDays] = React.useState<"1" | "3" | "7">("1");
+  const [text, setText] = useState("");
+  const [minChars, setMinChars] = useState("1");
+  const [maxChars, setMaxChars] = useState("500");
+  const [days, setDays] = useState("1");
   const provider = useProvider();
   const { data: account } = useAccount();
-  const { data, write } = useContractWrite(
+
+  const {
+    data,
+    write,
+    isLoading: isLoadingWrite,
+  } = useContractWrite(
     {
       addressOrName: PROMPTY_ADDRESS,
       contractInterface: ABI,
@@ -36,10 +43,12 @@ const Create = () => {
       ],
     }
   );
-  const hash = data?.hash;
-  useWaitForTransaction({
+
+  const { isLoading: isTransactionLoading } = useWaitForTransaction({
+    enabled: Boolean(data?.hash),
     confirmations: 4,
-    hash,
+    hash: data?.hash,
+    wait: data?.wait,
     onError(err) {
       console.error("error waiting for tx", err);
     },
@@ -64,8 +73,6 @@ const Create = () => {
     },
   });
 
-  console.log("hash", hash);
-
   const submitPrompt = async () => {
     if (provider) {
       write();
@@ -73,6 +80,12 @@ const Create = () => {
       console.log("no provider");
     }
   };
+
+  const handleDayChange = (_: any, value: string) => {
+    setDays(value);
+  };
+
+  const isLoading = isLoadingWrite || isTransactionLoading;
 
   return (
     <MainLayout>
@@ -85,7 +98,7 @@ const Create = () => {
           placeholder="Write your prompt..."
           onChange={(e) => setText(e.target.value)}
           value={text}
-          // disabled={!account?.address}
+          disabled={isLoading}
         />
         {account && (
           <div className="absolute bottom-4 left-4 py-2 text-sm font-bold text-gray-400">
@@ -94,63 +107,36 @@ const Create = () => {
         )}
       </div>
 
-      {hash && (
-        <div className="animate-spin text-6xl bg-gradient-to-r from-cyan-500 to-blue-500 p-10 rounded-full">
-          tx submitted bub...
-        </div>
-      )}
-
       {/* @ts-ignore */}
-      <form onChange={(e) => setDays(e.target.value)}>
+      <form>
         <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">
           How long should the prompt run?
         </h4>
         <div className="flex mb-8">
-          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
-            <input
-              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-              type="radio"
-              id="flexRadioDefault1"
-              checked={days === "1"}
-              value="1"
-            />
-            <label
-              className="form-check-label inline-block text-gray-800 cursor-pointer "
-              htmlFor="flexRadioDefault1"
-            >
-              24 hours
-            </label>
-          </div>
-          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
-            <input
-              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-              type="radio"
-              id="flexRadioDefault3"
-              checked={days === "3"}
-              value="3"
-            />
-            <label
-              className="form-check-label inline-block text-gray-800 cursor-pointer"
-              htmlFor="flexRadioDefault3"
-            >
-              3 days
-            </label>
-          </div>
-          <div className="form-check rounded-lg bg-white px-4 py-3 mr-4">
-            <input
-              className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-              type="radio"
-              id="flexRadioDefault7"
-              checked={days === "7"}
-              value="7"
-            />
-            <label
-              className="form-check-label inline-block text-gray-800 cursor-pointer"
-              htmlFor="flexRadioDefault7"
-            >
-              7 days
-            </label>
-          </div>
+          <RadioButton
+            disabled={isLoading}
+            id="flexRadioDefault1"
+            label="24 Hours"
+            value="1"
+            checked={days === "1"}
+            onClick={handleDayChange}
+          />
+          <RadioButton
+            disabled={isLoading}
+            id="flexRadioDefault3"
+            label="3 Days"
+            value="3"
+            checked={days === "3"}
+            onClick={handleDayChange}
+          />
+          <RadioButton
+            disabled={isLoading}
+            id="flexRadioDefault7"
+            label="7 Days"
+            value="7"
+            checked={days === "7"}
+            onClick={handleDayChange}
+          />
         </div>
       </form>
 
@@ -163,23 +149,32 @@ const Create = () => {
           className="border border-gray-300 p-2 mx-2 w-16"
           value={minChars}
           onChange={(e) => setMinChars(e.target.value)}
+          disabled={isLoading}
         />
         <label className="block text-gray-700 text-sm">Max Chars</label>
         <input
           className="border border-gray-300 p-2 mx-2 w-16"
           value={maxChars}
           onChange={(e) => setMaxChars(e.target.value)}
+          disabled={isLoading}
         />
       </div>
 
       <button
-        onClick={() => {
-          submitPrompt();
-        }}
-        className="rounded-full px-5 py-2 bg-orange-500 text-white text-sm font-bold"
+        disabled={isLoading}
+        onClick={submitPrompt}
+        className="rounded-full px-5 py-2 bg-orange-500 text-white text-sm font-bold disabled:opacity-50"
         type="submit"
       >
-        {"Post your Prompt"}
+        {isLoading ? (
+          <div className="flex">
+            <Spinner />
+            {isLoadingWrite ? "Submitting..." : ""}
+            {isTransactionLoading ? "TX Loading..." : ""}
+          </div>
+        ) : (
+          "Post your Prompt"
+        )}
       </button>
       {/* </form> */}
 
