a25c38fafc57a0735ab5e41102cbfb0ec8a0e27c
Dave Gorum <calculator@gmail.com>
2022-08-04 19:19:41 -0500

    style the landing page

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,15 +1,17 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { gql } from "urql";
-import MainLayout from "../layouts/MainLayout";
-import { usePublicPromptyInstancesQuery } from "../codegen/subgraph";
-import Link from "next/link";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { gql } from 'urql';
+import MainLayout from '../layouts/MainLayout';
+import { usePublicPromptyInstancesQuery } from '../codegen/subgraph';
+import Link from 'next/link';
+import Image from 'next/image';
 
 gql`
   query PublicPromptyInstances {
     promptyInstances(first: 100) {
       id
       name
+      description
       allowedResponders {
         id
       }
@@ -20,26 +22,54 @@ gql`
 const HomePage: NextPage = () => {
   // eslint-disable-next-line no-unused-vars
   const [query] = usePublicPromptyInstancesQuery(
-    typeof window === "undefined" ? { pause: true } : {}
+    typeof window === 'undefined' ? { pause: true } : {}
   );
 
   return (
     <>
       <Head>
-        <title>Welcome to Prompty</title>
+        <title>Welcome to Prompty!</title>
       </Head>
 
       <MainLayout>
-        <span>these are all the public prompty groups!</span>
-        <br></br>
-        <Link href="/group/create">
+        <h2 className="text-center text-2xl text-orange-500 font-bold mb-7">
+          Prompty is a collaborative prompting
+          <br />
+          game system for together fun.
+        </h2>
+
+        {/* <Link href="/group/create">
           <a className="p-2 bg-gray-500">Create a new group</a>
-        </Link>
+        </Link> */}
 
         {query.data?.promptyInstances.map((p: any) => (
-          <div className="p-4 bg-gray-400" key={p.id}>
-            <Link href={`/group/${p.id}`}>{p.name}</Link>
-          </div>
+          <Link href={`/group/${p.id}`}>
+            <div
+              className="p-4 pb-2 mb-5 bg-white flex w-full cursor-pointer rounded-xl "
+              key={p.id}
+            >
+              <img
+                src="/assets/img-group-buzzard.png"
+                className="w-12 h-12 rounded-sm mr-4"
+              />
+              <div className="flex flex-col w-full">
+                <div className="flex flex-row mb-2">
+                  <h2 className="grow font-bold text-orange-500">{p.name}</h2>
+                  <span className="flex flex-row mr-2">
+                    <small className="whitespace-nowrap mr-2 opacity-60">
+                      3 active prompts
+                    </small>
+                    <img
+                      src="/assets/img-groupStatus-global.svg"
+                      className="w-5"
+                    />
+                  </span>
+                </div>
+
+                <p>This is a description of the life purpose of the group.</p>
+              </div>
+            </div>
+          </Link>
         ))}
       </MainLayout>
     </>
