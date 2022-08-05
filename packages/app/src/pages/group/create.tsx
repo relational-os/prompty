03fa6f541cea4a844546781e785417e7efbb4193
Dave Gorum <calculator@gmail.com>
2022-08-05 11:40:37 -0500

    update create group styles

--- packages/app/src/pages/group/create.tsx
+++ packages/app/src/pages/group/create.tsx
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
-import Spinner from "src/components/Spinner";
-import MainLayout from "src/layouts/MainLayout";
-import { useElapsedTime } from "use-elapsed-time";
+import React, { useState } from 'react';
+import Spinner from 'src/components/Spinner';
+import MainLayout from 'src/layouts/MainLayout';
+import { useElapsedTime } from 'use-elapsed-time';
 // import { ENSName } from "react-ens-name";
 import {
   useAccount,
   useContractWrite,
   useProvider,
   useWaitForTransaction,
-} from "wagmi";
-import { useRouter } from "next/router";
-import { defaultAbiCoder } from "ethers/lib/utils";
-import { ABI, PROMPTY_ADDRESS } from "src/contracts";
+} from 'wagmi';
+import { useRouter } from 'next/router';
+import { defaultAbiCoder } from 'ethers/lib/utils';
+import { ABI, PROMPTY_ADDRESS } from 'src/contracts';
 
-const PENDING_TRANSACTION_LOADING_MESSAGE = "tx processing...";
-const PENDING_WRITE_LOADING_MESSAGE = "Sign the message...";
+const PENDING_TRANSACTION_LOADING_MESSAGE = 'tx processing...';
+const PENDING_WRITE_LOADING_MESSAGE = 'Sign the message...';
 const Create = () => {
   const router = useRouter();
 
   const provider = useProvider();
   const { data: account } = useAccount();
 
-  const [groupName, setGroupName] = useState("");
-  const [description, setDescription] = useState("");
+  const [groupName, setGroupName] = useState('');
+  const [description, setDescription] = useState('');
+  const [visibility, setVisibility] = useState<string>('public');
   // eslint-disable-next-line no-unused-vars
   const [allowList, setAllowList] = useState([account?.address]);
   // const [visibility, setVisibility] = useState("public");
@@ -31,7 +32,7 @@ const Create = () => {
     if (provider) {
       write();
     } else {
-      console.log("no provider");
+      console.log('no provider');
     }
   };
 
@@ -44,7 +45,7 @@ const Create = () => {
       addressOrName: PROMPTY_ADDRESS,
       contractInterface: ABI,
     },
-    "createInstance",
+    'createInstance',
     {
       args: [allowList, groupName, description],
     }
@@ -56,21 +57,21 @@ const Create = () => {
     hash: data?.hash,
     wait: data?.wait,
     onError(err) {
-      console.error("error waiting for tx", err);
+      console.error('error waiting for tx', err);
     },
     onSuccess(data) {
       // here: redirect to the page
-      console.log("success", data);
+      console.log('success', data);
 
       const event = defaultAbiCoder.decode(
         [
-          "uint256",
-          "address",
-          "string",
-          "uint256",
-          "uint256",
-          "uint128",
-          "uint128",
+          'uint256',
+          'address',
+          'string',
+          'uint256',
+          'uint256',
+          'uint128',
+          'uint128',
         ],
         data.logs[0].data
       );
@@ -84,19 +85,47 @@ const Create = () => {
 
   return (
     <MainLayout>
-      <span>Create your Group!</span>
+      <div className="w-8/12 mx-auto min-w-fit">
+        <h1 className="text-center text-2xl mb-4">Create your Group!</h1>
 
-      <div className="flex flex-col">
-        <label>group name</label>
-        <input
-          type="text"
-          onChange={(e) => setGroupName(e.target.value)}
-        ></input>
-        <label>description</label>
-        <input
-          type="text"
-          onChange={(e) => setDescription(e.target.value)}
-        ></input>
+        <div className="mb-5 w-full">
+          <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">Group Name</h4>
+          <input
+            type="text"
+            className="w-full px-2 py-1 border-2 border-gray-200 rounded-md placeholder:text-gray-400"
+            onChange={(e) => setGroupName(e.target.value)}
+          ></input>
+        </div>
+
+        <div className="mb-5 w-full">
+          <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">
+            Description
+          </h4>
+          <textarea
+            rows={3}
+            className="w-full p-2 border-2 border-gray-200 rounded-md placeholder:text-gray-400"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+
+        <div className="mb-5 ">
+          <h4 className="font-bold mb-3 text-sm ml-2 opacity-70">Visibility</h4>
+          <select
+            className="w-full p-2 border-2 border-gray-200 rounded-md placeholder:text-gray-400 cursor-pointer"
+            onChange={(e) => {
+              setVisibility(e.target.value);
+            }}
+          >
+            <option value="public" selected={visibility == 'public'}>
+              Public
+            </option>
+            <option value="unlisted" selected={visibility == 'unlisted'}>
+              Unlisted
+            </option>
+          </select>
+        </div>
+
         <button
           disabled={isLoading}
           onClick={submitGroup}
@@ -106,15 +135,15 @@ const Create = () => {
           {isLoading ? (
             <div className="flex">
               <Spinner />
-              {isLoadingWrite ? PENDING_WRITE_LOADING_MESSAGE : ""}
+              {isLoadingWrite ? PENDING_WRITE_LOADING_MESSAGE : ''}
               {isTransactionLoading
                 ? `${PENDING_TRANSACTION_LOADING_MESSAGE} ${Math.round(
                     elapsedTime
                   )}s`
-                : ""}
+                : ''}
             </div>
           ) : (
-            "create group"
+            '+ Create Group'
           )}
         </button>
       </div>
